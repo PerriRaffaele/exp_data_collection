0437af877af5029a2447883ff6f6f3c78fffc0c9
Raffaele <raffaele.perri@usi.ch>
2024-03-07 00:17:15 +0100

    trying to solve a connection problem

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const fs = require('fs');
 
 
 const app = express();
-app.use(cors({
-    origin: ["https://exp-data-collection-frontend.vercel.app"],
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://exp-data-collection-frontend.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(express.json());
 
