cd9176092a195ae122a1f5e196729446088707c7
Raffaele <raffaele.perri@usi.ch>
2024-03-07 16:57:44 +0100

    removing useless

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "https://exp-data-collection-frontend.vercel.app"}})
+CORS(app)
 
 @app.route('/exercises', methods=['GET'])
 # HERE CODE CALLED FROM THE FRONTEND FILE ExperimentViewUno.vue
