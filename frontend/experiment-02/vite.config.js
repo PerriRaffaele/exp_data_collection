f4c41446bcc40b1aecfe48318b8e6b255b2d4df7
Raffaele <raffaele.perri@usi.ch>
2024-03-07 10:13:18 +0100

    trying to get the backend

--- frontend/experiment-02/vite.config.js
+++ frontend/experiment-02/vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
   plugins: [vue()],
   assetsInclude: ['**/*.png', '**/*.PNG', '**/*.jpg', '**/*.svg', '**/*.gif', '**/*.mp3', '**/*.wav', '**/*.ogg', '**/*.json'],
   publicDir: './public', // Specify the path to your public folder
+  module: {
+    async headers() {
+      return [
+        {
+          source: '../../backend/:path*', // Specify the path to your backend folder
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET, POST, PATCH, PUT, DELETE, OPTIONS" },
+            { key: "Access-Control-Allow-Headers", value: "Origin, X-Requested-With, Content-Type, Accept, Authorization" }
+          ]
+        }
+      ]
+    }
+  }
 })
