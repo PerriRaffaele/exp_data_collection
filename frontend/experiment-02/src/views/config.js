c56b42839494964287a487fe187d10f3db6e9e9a
Raffaele <raffaele.perri@usi.ch>
2024-03-12 16:47:53 +0100

    new backend

--- frontend/experiment-02/src/views/config.js
+++ frontend/experiment-02/src/views/config.js
@@ -1,6 +1,6 @@
 const config = {
     local_backend: "http://localhost:3000",
-    production_backend: "https://exp-data-backend.vercel.app",
+    production_backend: "https://data-backend-762y.onrender.com",
 };
 
 export default config;
